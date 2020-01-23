e9ce066aed57521eee3c88454bb13883423422c3
Honglei <emmanueldollinger@Harry.local>
2020-01-22 17:19:09 -0800

    01/22/20

--- python-code/prepare_data.py
+++ python-code/prepare_data.py
@@ -3,12 +3,15 @@ import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from scipy.stats import pearsonr
 
 data_dir = "../data"
 chromHMM_labels = range(1, 16)
 distance_range = range(2, 1000 + 1)
 chromosomes = ["chr%d" % i for i in range(1, 23)]
-
+FEATURE_NAME = "feature"
+CORR_NAME = "corr"
 def mkdir(dir_path):
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
@@ -25,15 +28,27 @@ def step1_split_bed_file_by_chromHMM_label(input_bed_fp, outdir, col_index=-1):
             print("save %s sucessful" % chrHMMlbl)
     except Exception as e:
         print(e)
+def partition_CpG_pairs_dict_into_training_validation_and_testing_set(CpG_pair_withd_chrHMM, training_ratio):
+    CpG_pair_withd_chrHMM = CpG_pair_withd_chrHMM[0]
+
+    training_set = {chrHMMlbl: {d: {FEATURE_NAME:[]} for d in distance_range} for chrHMMlbl in chromHMM_labels}
+    testing_set = {chrHMMlbl: {d: {FEATURE_NAME:[]} for d in distance_range} for chrHMMlbl in chromHMM_labels}
+
+    for chrHMMlbl in chromHMM_labels:
+        for d in distance_range:
+            feature_vec_list = CpG_pair_withd_chrHMM[chrHMMlbl][d]
+            if len(feature_vec_list):
+                training_set[chrHMMlbl][d][FEATURE_NAME], testing_set[chrHMMlbl][d][FEATURE_NAME] = train_test_split(feature_vec_list, train_size=training_ratio)
+    return [training_set], [testing_set]
+
 def step2_construct_cpg_pairs_with_d_distance(input_dir):
     CpG_pair_withd_chrHMM = {} # the dict for storing the data of CpG pairs with d distance, for each chromHMM type.
     for chrHMMlbl in chromHMM_labels:
-        CpG_pair_withd_chrHMM[chrHMMlbl] = {d: [] for d in distance_range}
+        CpG_pair_withd_chrHMM[chrHMMlbl] = {d: {FEATURE_NAME:[]} for d in distance_range}
         input_bed_fp = os.path.join(input_dir, "%s.bed" % chrHMMlbl)
         df = pd.read_csv(input_bed_fp, sep='\t', header=None).values
         for chrm in chromosomes:
             df_lines = df[df[:, 0] == chrm, :]
-
             CpG_pos_and_data_dict = {}
             for item in df_lines:
                 pos = item[1]
@@ -44,12 +59,47 @@ def step2_construct_cpg_pairs_with_d_distance(input_dir):
                     pos_off_by_d = pos + d
                     if pos_off_by_d in CpG_pos_and_data_dict.keys():
                         feature_of_CpG_pairs = [CpG_pos_and_data_dict[pos], CpG_pos_and_data_dict[pos_off_by_d]]
-                        CpG_pair_withd_chrHMM[chrHMMlbl][d].append(feature_of_CpG_pairs)
+                        CpG_pair_withd_chrHMM[chrHMMlbl][d][FEATURE_NAME].append(feature_of_CpG_pairs)
+    training_set, testing_set = partition_CpG_pairs_dict_into_training_validation_and_testing_set([CpG_pair_withd_chrHMM], training_ratio=0.5)
+    return training_set, testing_set
 
+def local_feature_keys(d, local_radius):
+    if (d - local_radius) not in distance_range:
+        ds = range(d, d + 2 * local_radius + 1)
+    elif (d + local_radius + 1) not in distance_range:
+        ds = range(d - 2 * local_radius + 1, d)
+    else:
+        ds = range(d - local_radius, d + local_radius + 1)
+    return ds
+
+
+def calc_local_correlation(dataset, local_radius, target_feature_col = 5, NUM_MIN_SAMPLE = 5):
+    dataset = dataset[0]
+    COL_INDEX_IN_DF = target_feature_col - 3 - 1
+    for chrHMMlbl in chromHMM_labels:
+        for d in distance_range:
+            ds = local_feature_keys(d, local_radius)
+            vec_list = []
+            for dd in ds:
+                feature_data = dataset[chrHMMlbl][dd][FEATURE_NAME]
+                if feature_data:
+                    vec_list.append([feature_data[0][COL_INDEX_IN_DF], feature_data[1][COL_INDEX_IN_DF]])
+            if len(vec_list) > NUM_MIN_SAMPLE:
+                vec_list = np.array(vec_list)
+                corr, _ = pearsonr(vec_list[:, 0], vec_list[:, 1])
+                dataset[chrHMMlbl][d][CORR_NAME] = corr
+            else:
+                dataset[chrHMMlbl][d][CORR_NAME] = -1
+    return [dataset]
+def step3_get_local_correlation_for_each_CpG_pair(training_set, testing_set, local_radius= 2):
+    training_set = calc_local_correlation(training_set, local_radius)
+    testing_set = calc_local_correlation(testing_set, local_radius)
+    
 if __name__ == "__main__":
 
     input_bed_fp = os.path.join(data_dir, "K-100bp-CpGd_HMM.bed")
     outdir = os.path.join(data_dir, "K-100bp-CpGd_split")
     #step1_split_bed_file_by_chromHMM_label(input_bed_fp, outdir)
 
-    step2_construct_cpg_pairs_with_d_distance(outdir)
+    training_set, testing_set = step2_construct_cpg_pairs_with_d_distance(outdir)
+    step3_get_local_correlation_for_each_CpG_pair(training_set, testing_set)
